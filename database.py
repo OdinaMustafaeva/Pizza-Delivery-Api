a276d5b92fd8533825a714dd2ec2c625f69a1044
Odina <113756535+OdinaMustafaeva@users.noreply.github.com>
2023-06-07 11:18:30 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,15 +1,10 @@
 from dotenv import dotenv_values
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-
-env = dotenv_values(".env")
-
+env dotenv_values(".env")
 DB_NAME = env.get("DB_NAME")
 DB_USER = env.get("DB_USER")
-DB_PASSWORD = env.get("DB_PASSWORD")
-SQL_ALCHEMY_DB_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@localhost/{DB_NAME}'
-engine = create_engine(SQL_ALCHEMY_DB_URL, echo=True)
-
-Base = declarative_base()
-
-Session = sessionmaker()
+DB_PASSWORD= env.get("DB_PASSWORD")
+SQL ALCHEMY_DB_URL = f'sqlite:///./app.db'
+engine create engine (SQL_ALCHEMY_DB_URL, connect_args=("check_same_thread": False}, echo=True)
+Base declarative_base()
